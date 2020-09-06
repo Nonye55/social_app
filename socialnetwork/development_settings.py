738d23787cd7d9292b0ef267c84d202957c1a21c
Nonye55 <59821061+Nonye55@users.noreply.github.com>
2020-09-06 03:24:16 +0100

    decoupled

--- socialnetwork/development_settings.py
+++ socialnetwork/development_settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import django_heroku
 import os
 import environ
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from decouple import config
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env()
